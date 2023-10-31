28cc79001a843fd1d13c57671f5c53a98bccb1ae
Lucas Santos da Costa <lsantosc@live.com>
2023-10-31 01:39:54 -0300

    Hi 👋, this is the start 🚀

---
+++ lib/tabler/rails.rb
@@ -0,0 +1,8 @@
+require "tabler/rails/version"
+require "tabler/rails/engine"
+
+module Tabler
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/tabler/rails/engine.rb
@@ -0,0 +1,6 @@
+module Tabler
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end
